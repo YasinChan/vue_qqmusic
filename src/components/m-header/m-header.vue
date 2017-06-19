628fba96aa1ed9af791e0588af63dc7dd01d4dc1
yasinchen <yasinchan2016@gmail.com>
2017-06-20 03:07:35 +0800

    finish recommend singer components and so on

---
+++ src/components/m-header/m-header.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="m-header">
+    <div class="icon"></div>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+  export default {}
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+  @import "~common/stylus/variable"
+
+  .m-header
+    position: relative
+    height: 44px
+    text-align: center
+    color: $color-theme
+    font-size: 0
+    .icon
+      display: inline-block
+      vertical-align: top
+      margin-top: 6px
+      width: 115px
+      height: 32px
+      margin-right: 9px
+      background url("logo.png")
+      background-size: 115px 32px
+    .mine
+      position: absolute
+      top: 0
+      right: 0
+      .icon-mine
+        display: block
+        padding: 12px
+        font-size: 20px
+        color: $color-theme
+</style>
